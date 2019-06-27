b1fa9c04aaa7c362a318067439d6354cf360a7d3
msicil <mikejoesis@gmail.com>
2019-06-27 15:57:44 -0400

    added heatmap

--- client/components/basemap.js
+++ client/components/basemap.js
@@ -1,25 +1,31 @@
 /* eslint-disable react/display-name */
 import React from 'react'
-import {Scene} from '@esri/react-arcgis'
+import {Map, Scene} from '@esri/react-arcgis'
 import BoroughLayer from './borough-layer'
 import CrimeLayer from './crime-layer'
 import BuildingLayer from './building-layer'
 import LayerList from './layerlistwidget'
+import CrimeHeat from './crime-heatmap'
+
+const loaderOptions = {
+  url: 'http://js.arcgis.com/4.11'
+}
 
 export default props => {
   return (
-    <Scene
+    <Map
       style={{width: '100vw', height: '100vh'}}
       mapProperties={{basemap: 'dark-gray-vector'}}
       viewProperties={{
         center: [-73.953413, 40.788602],
         zoom: 12.5
       }}
+      loaderOptions={loaderOptions}
     >
       <LayerList />
       <BoroughLayer />
-      <CrimeLayer />
+      <CrimeHeat />
       <BuildingLayer />
-    </Scene>
+    </Map>
   )
 }
