aa7549b474e88dd2f337f82f95541fefd7ba88cb
Alex Mann <Alexander_Mann@alumni.brown.edu>
2019-06-28 11:40:36 -0400

    feat data pulled variant with current time

--- client/components/crime-heatmap.js
+++ client/components/crime-heatmap.js
@@ -1,6 +1,9 @@
 import {useState, useEffect} from 'react'
 import {loadModules} from '@esri/react-arcgis'
 
+let currentTime = new Date()
+let currentHour = currentTime.getHours()
+
 const CrimeHeat = props => {
   const [layer, setLayer] = useState(null)
   const renderer = {
@@ -28,8 +31,9 @@ const CrimeHeat = props => {
     loadModules(['esri/layers/GeoJSONLayer'])
       .then(([GeoJSONLayer]) => {
         let initLayer = new GeoJSONLayer({
-          url:
-            'https://data.cityofnewyork.us/resource/9s4h-37hy.geojson?$where=cmplnt_fr_dt%20between%20%272018-01-01%27%20and%20%272018-12-31%27%20AND%20cmplnt_fr_tm%20between%20%2701:00:00%27%20and%20%2701:15:00%27&$select=CMPLNT_FR_DT,CMPLNT_FR_TM,LAW_CAT_CD,Lat_Lon&$limit=50000',
+          url: `https://data.cityofnewyork.us/resource/9s4h-37hy.geojson?$where=cmplnt_fr_dt%20between%20%272018-01-01%27%20and%20%272018-12-31%27%20AND%20cmplnt_fr_tm%20between%20%27${currentHour -
+            1}:00:00%27%20and%20%27${currentHour +
+            1}:15:00%27&$select=CMPLNT_FR_DT,CMPLNT_FR_TM,LAW_CAT_CD,Lat_Lon&$limit=50000`,
           renderer,
           title: 'Crime Heat Map'
         })
