58f1544cfd7bf7bb5a0ad43e0d01972e68f6cbe6
Alex Mann <Alexander_Mann@alumni.brown.edu>
2019-07-01 17:01:37 -0400

    refactoring, edit ReadMe

--- client/components/crime-heatmap.js
+++ client/components/crime-heatmap.js
@@ -36,7 +36,7 @@ const CrimeHeat = props => {
           const template = {
             title: '{PD_DESC}',
             content:
-              '<p><b>Specifcs:</b> {OFNS_DESC}</p> <b>Date:</b> {CMPLNT_FR_DT:DateString} <b>Time:</b>{CMPLNT_FR_TM} <b>'
+              '<p><b>Crime:</b> {OFNS_DESC}</p> <b>Date:</b> {CMPLNT_FR_DT:DateString(hideTime: true).substring(0,11)} <b>Time:</b>{CMPLNT_FR_TM} <b>'
           }
           let initLayer = new GeoJSONLayer({
             url: `https://data.cityofnewyork.us/resource/9s4h-37hy.geojson?$where=cmplnt_fr_dt%20between%20%272018-01-01%27%20and%20%272018-12-31%27%20AND%20cmplnt_fr_tm%20between%20%27${props.currentHour -
@@ -45,7 +45,6 @@ const CrimeHeat = props => {
             renderer: heatMapRenderer,
             title: 'Crime Heat Map'
           })
-          console.log(props.currentHour)
           setLayer(initLayer)
           props.map.add(initLayer)
 
