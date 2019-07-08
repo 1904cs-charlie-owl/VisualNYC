60bf9312e586817fe310daed0c910862b77f433d
Ricky Lau <rickylaufitness@gmail.com>
2019-07-08 15:14:15 -0400

    refactored from html and changes view upon borough click

--- client/components/boroughs-layer.js
+++ client/components/boroughs-layer.js
@@ -6,21 +6,6 @@ const BoroughsLayer = props => {
   useEffect(() => {
     loadModules(['esri/layers/FeatureLayer', 'esri/WebScene'])
       .then(([FeatureLayer]) => {
-        // let boroughRenderer = {
-        //   type: 'simple',
-        //   symbol: {
-        //     type: 'simple-marker',
-        //     color: '#c80000',
-        //     size: 10
-        //   }
-        // }
-
-        let initLayer = new FeatureLayer({
-          url:
-            'https://services9.arcgis.com/UBBAhYgiEL7Yaa7P/arcgis/rest/services/boroughs/FeatureServer'
-        })
-
-        // var highlightSelect
         const boroughCoords = {
           Brooklyn: [40.6782, -73.9442],
           Manhattan: [40.7831, -73.9712],
@@ -28,12 +13,46 @@ const BoroughsLayer = props => {
           'Staten Island': [40.5795, -74.1502],
           Bronx: [40.8448, -73.8648]
         }
-        props.view.when(function() {
-          let buttons = document.getElementsByClassName('boroughs')
 
-          for (let i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener('click', onClick)
+        const paneDiv = document.createElement('div')
+        paneDiv.id = 'paneDiv'
+        paneDiv.setAttribute(
+          'style',
+          'position:absolute; display:flex; margin-left:100px; top:40px; width: 50%; text-align:center; background-color: transparent'
+        )
+        document.body.appendChild(paneDiv)
+
+        for (let i = 0; i < 5; i++) {
+          let button = document.createElement('button')
+          button.className = 'esri-button esri-button-overwrite boroughs'
+          button.setAttribute(
+            'style',
+            'width:10%; display:table-cell; margin:4px; background-color: #69dcff'
+          )
+          paneDiv.appendChild(button)
+        }
+
+        const boroughButtons = document.getElementsByClassName('boroughs')
+
+        boroughButtons[0].innerText = 'Brooklyn'
+        boroughButtons[1].innerText = 'Manhattan'
+        boroughButtons[2].innerText = 'Queens'
+        boroughButtons[3].innerText = 'Staten Island'
+        boroughButtons[4].innerText = 'Bronx'
+
+        props.view.ui.add(paneDiv)
+
+        let initLayer = new FeatureLayer({
+          url:
+            'https://services9.arcgis.com/UBBAhYgiEL7Yaa7P/ArcGIS/rest/services/boroughs/FeatureServer',
+          visible: false
+        })
+
+        props.view.when(function() {
+          for (let i = 0; i < boroughButtons.length; i++) {
+            boroughButtons[i].addEventListener('click', onClick)
           }
+
           let queryBoroughs = initLayer.createQuery()
 
           function onClick(event) {
